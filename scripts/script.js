5cb99d32247b835ed72941ae9ebc5e54be8d3da0
Pauline <lynnsulit@gmail.com>
2022-01-31 22:10:11 -0500

    animated hamburger menu

--- scripts/script.js
+++ scripts/script.js
@@ -1,19 +1,12 @@
-// Create a drop down menu when hamburger menu is clicked
+// Hamburger Menu
 
-// 1. Create a variable for the hamburger menu
-// 2. Display none main menu and show hamburger menu
-// 3. Add an event listener, specifically listening for the hamburger menu when clicked
-// 4. When clicked, show the navBar
-// 5. Add transition from hamburger menu to X icon
-// 6. When X is clicked, hide navBar
-// <i class="fas fa-times"></i>
-
-const hamMenu = document.querySelector(".fa-bars");
-
+const menuBtn = document.querySelector(".menuBtn");
 const navMenu = document.querySelector("#mainNav");
 
-hamMenu.addEventListener("click", () => {
+menuBtn.addEventListener("click", () => {
+  menuBtn.classList.toggle("open");
   navMenu.classList.toggle("showMenu");
 });
 
+// scroll animations
 AOS.init();
