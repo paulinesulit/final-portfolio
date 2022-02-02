aed8e64e6eaea99c5acdfc03138926a41ff5cd5b
Pauline <lynnsulit@gmail.com>
2022-02-02 18:02:14 -0500

    more js for navbar :tada:

--- scripts/script.js
+++ scripts/script.js
@@ -3,10 +3,22 @@
 const menuBtn = document.querySelector(".menuBtn");
 const navMenu = document.querySelector("nav");
 
-menuBtn.addEventListener("click", () => {
+const about = document.querySelector("#aboutLink");
+const skills = document.querySelector("#skillsLink");
+const projects = document.querySelector("#projectsLink");
+const contact = document.querySelector("#contactLink");
+
+const menuToggle = () => {
   menuBtn.classList.toggle("open");
   navMenu.classList.toggle("showMenu");
-});
+};
+
+menuBtn.addEventListener("click", menuToggle);
+about.addEventListener("click", menuToggle);
+skills.addEventListener("click", menuToggle);
+projects.addEventListener("click", menuToggle);
+contact.addEventListener("click", menuToggle);
+
 
 // scroll animations
 AOS.init();
